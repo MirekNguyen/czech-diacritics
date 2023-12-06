ea76a7a77f414f0452c98d632b4e4a6d8769efd3
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-12-06 02:06:12 +0100

    feat: use nvim user command

--- plugin/czech-diacritics.lua
+++ plugin/czech-diacritics.lua
@@ -1,1 +1,6 @@
-vim.cmd([[command! -range AddDiacritics lua require('czech-diacritics').add_diacritics()]])
+vim.api.nvim_create_user_command("AddDiacritics", function()
+	require("czech-diacritics").add_diacritics()
+end, {
+	range = true,
+})
+
